610570672c9b1ad4b41972d4d206ca3086cacdd8
prajkta <prajkta@globaltestingexperts.com>
2022-01-24 20:52:34 +0530

    model validations and rubocop autofixes

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,51 +1,58 @@
-class Api::V1::TagsController < ApplicationController
-  before_action :set_tag, only: %i[ show update destroy ]
+# frozen_string_literal: true
 
-  # GET /tags
-  def index
-    @tags = Tag.all
+module Api
+  module V1
+    class TagsController < ApplicationController
+      before_action :set_tag, only: %i[show update destroy]
 
-    render json: @tags
-  end
+      # GET /tags
+      def index
+        @tags = Tag.all
 
-  # GET /tags/1
-  def show
-    render json: @tag
-  end
+        render json: @tags
+      end
 
-  # POST /tags
-  def create
-    @tag = Tag.new(tag_params)
+      # GET /tags/1
+      def show
+        render json: @tag
+      end
 
-    if @tag.save
-      render json: @tag, status: :created, location: @tag
-    else
-      render json: @tag.errors, status: :unprocessable_entity
+      # POST /tags
+      def create
+        @tag = Tag.new(tag_params)
+
+        if @tag.save
+          render json: @tag, status: :created, location: @tag
+        else
+          render json: @tag.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /tags/1
+      def update
+        if @tag.update(tag_params)
+          render json: @tag
+        else
+          render json: @tag.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /tags/1
+      def destroy
+        @tag.destroy
+      end
+
+      private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_tag
+        @tag = Tag.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def tag_params
+        params.require(:tag).permit(:name, :tag_type, :description)
+      end
     end
   end
-
-  # PATCH/PUT /tags/1
-  def update
-    if @tag.update(tag_params)
-      render json: @tag
-    else
-      render json: @tag.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /tags/1
-  def destroy
-    @tag.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tag
-      @tag = Tag.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def tag_params
-      params.require(:tag).permit(:name, :tag_type, :description)
-    end
 end
